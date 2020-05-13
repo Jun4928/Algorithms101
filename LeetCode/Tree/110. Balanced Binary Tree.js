8f5903486e3033482521c7c8f89925af28619266
Jun4928 <jun.choi.4928@gmail.com>
2020-05-14 00:57:36 +0900

    20.05.14

--- LeetCode/Tree/110. Balanced Binary Tree.js
+++ LeetCode/Tree/110. Balanced Binary Tree.js
@@ -11,61 +11,16 @@
  * @return {boolean}
  */
 
-class BinaryTreeNode {
-    constructor(val) {
-        this.val = val; 
-        this.left = null; 
-        this.right = null; 
-    }
-}
-
-// const bfs = function BreadthFirstSearch (node) {
-//     const unvisitedQueue = [node, 'LEVEL'];
-//     let height = 1;
-
-//     while(unvisitedQueue.length !== 1) {
-//         const current = unvisitedQueue.shift();
-
-//         if(current === 'LEVEL') {
-//             height += 1;
-//             unvisitedQueue.push(current);
-//         }
-
-//         if(current !== 'LEVEL') {
-//             if(current.left) unvisitedQueue.push(current.left);
-//             if(current.right) unvisitedQueue.push(current.right);
-//         }
-//     }
-//     return height;
-// }
-
 const isBalanced = function toDetermineHeightBalanced (root) {
 
-    const dfs = function (node, heights = []) {
+    const dfs = function (node) {
         if(!node) return 0;
+        let left = dfs(node.left);
+        let right = dfs(node.right);
 
-        let left = dfs(node.left, heights);
-        let right = dfs(node.right, heights);
-
-        if(left === -1 || right === -1 || Math.abs(left - right) > 1) return -1; // unbalanced
-
-        return 1 + Math.max(left, right);
-
+        if(left === -1 || right === -1 || Math.abs(left - right) > 1) return -1; // unbalanced, call stack 타고 올라가서 최종적으로 -1 return 한다.
+        return 1 + Math.max(left, right); // 원래대로 라면, 트리의 가장 깊은 깊이를 return 한다.
     }
 
     return dfs(root) !== -1;
 };
-
-// const dfs = function toFindtheDepthofBinaryTree (node) { // 
-//     if(!node) return 0;
-//     let left = dfs(node.left);
-//     let right = dfs(node.right);
-//     return 1 + Math.max(left, right);
-// }
-
-const root1 = new BinaryTreeNode(3);
-root1.left = new BinaryTreeNode(9); 
-root1.right = new BinaryTreeNode(20); 
-root1.right.left= new BinaryTreeNode(15); 
-root1.right.right = new BinaryTreeNode(7); 
-console.log(isBalanced(root1));
