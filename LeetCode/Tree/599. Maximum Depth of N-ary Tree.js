5ca366f53233d4b668defa564a1d12fc52efb243
Jun4928 <jun.choi.4928@gmail.com>
2020-05-14 15:01:22 +0900

    20.05.14

--- LeetCode/Tree/599. Maximum Depth of N-ary Tree.js
+++ LeetCode/Tree/599. Maximum Depth of N-ary Tree.js
@@ -2,7 +2,7 @@
  * // Definition for a Node.
  * function Node(val,children) {
  *    this.val = val;
- *    this.children = children; // array
+ *    this.children = children;
  * };
  */
 
@@ -10,15 +10,18 @@
  * @param {Node} root
  * @return {number}
  */
-var maxDepth = function(root) { 
-    if(!root) return 0;  
-   
-    //const childrenDepth = root.children.map(child => maxDepth(child));
-    //const maxD = childrenDepth.reduce((max, depth) => depth > max ? depth : max , 0);
-    const maxD = root.children.reduce((max, child) => { // 위의 두 줄 map, reduce 를 한번에 구현한 것
-      const depth = maxDepth(child); 
-      return depth > max ? depth : max;
-    }, 0);
-    
-    return 1 + maxD;
-  };
+const maxDepth = function(root) {
+
+    const dfs = function ReturnMaxDepth(node)  {
+      if(!root) return 0;  
+
+      const maxD = node.children.reduce((max, child) => {
+        const depth = dfs(child); 
+        return depth > max ? depth : max;
+      }, 0);
+
+      return 1 + maxD;
+    }
+
+  return dfs(root);
+};
