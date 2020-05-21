22f915919a70629957adacee1e590e8ba4ad2d87
Jun4928 <jun.choi.4928@gmail.com>
2020-05-21 15:33:12 +0900

    20.05.21 modified

--- LeetCode/Tree/863. All Nodes Distance K in Binary Tree.js
+++ LeetCode/Tree/863. All Nodes Distance K in Binary Tree.js
@@ -23,47 +23,46 @@ const makeBinaryTree = function(array) { // array into Binary Tree
 }
 
 const distanceK = function(root, target, K) {
-  let graph = {}; // key: node.value, value: neighbor[parent.value, left.value, right.value];
+  let graph = {};
+  const dfs = function BinaryTreeIntoGraph (node, parent) {
+    if(node === null) return; // ending 조건 : null 일 때 그냥 return   
+    const neighbor = [];
+    neighbor.push(parent);
 
-  const dfs = function treeIntoGraph(node, parent, graph) {
-    if(!node) return; // ending condition
-    let neighbor = [parent, null, null]; // parent: parent.value -1 : null -1 : null initialized
+    node.left ? neighbor.push(node.left.val) : neighbor.push(null); // 있으면 값, 없으면 null push
+    dfs(node.left, node.val);
 
-    if(node.left !== null) {
-      neighbor[1] = node.left.val; // store left value
-      dfs(node.left, node.val, graph);
-    }
+    node.right ? neighbor.push(node.right.val) : neighbor.push(null); // 있으면 값, 없으면 null push
+    dfs(node.right, node.val);
 
-    if(node.right !== null) {
-      neighbor[2] = node.right.val; // store right value
-      dfs(node.right, node.val, graph);
-    }
-
-    graph[node.val] = neighbor; // graph update 
+    graph[node.val] = neighbor; // graph 에 저장 [parent, left.value, right.value];
   }
+  dfs(root, null);
+  console.log(graph);
 
-  dfs(root, null, graph); // root parent.value = -1
-
-  // Graph BFS Algorithm
-  const unvisitedQ = [{ value: target.val, distance: 0 }]; // value, distance;
-  const visited = new Set(); // to see if it's visited, Set helps, because it omits dupilication.
+  // Graph BFS Algorithm : using [Queue:unvisited and Set:visited]
+  const unvisitedQueue = [{ value: target.val, distance: 0 }];   
+  const visitedSet = new Set();
   const result = [];
 
-  while(unvisitedQ.length > 0) {
-    const {value, distance} = unvisitedQ.shift(); // destructure the current value and the current distance
+  while(unvisitedQueue.length > 0) {
+    const {value, distance} = unvisitedQueue.shift(); // 현재 탐색할 노드 Queue 에서 꺼냄
 
-    if(visited.has(value)) continue; // if this value has been visited, continue this while loop
-    visited.add(value); // otherwise, add this value into the visited Set
+    // if(value === null) continue; <= 이 구문을 넣었더니 성능이 심하게 저하되었다.
+    if(visitedSet.has(value)) continue;  // 미리 봤다면, 아래 과정을 거칠 필요가 없음
+    visitedSet.add(value); // 먼저 set 에 넣어준다. 다음번에 반복을 피하기 위해서
 
-    if(distance === K && value !== null) result.push(value); // found the node with the same distance K
-    if(distance < K && value !== null) {
-      const neighbor = graph[value];
-      neighbor.forEach(each => unvisitedQ.push({value: each, distance: distance + 1}));
+    if(distance === K && value !== null) result.push(value); // 거리가 같고, null 이 아니라면 push
+    if(distance < K && value !== null) { // 아직 거리에 도달하지 못 했다면, 탐색을 계속한다.
+      // 탐색을 지속한다는 의미는 queue 에다가 탐색 할 애들을 넣어주는 것
+      const neighbor = graph[value]; // neighbor array [parent, left, right];
+      neighbor.forEach( each => unvisitedQueue.push({value: each, distance: distance + 1 })); // distance 1을 증가시킨다. 
     }
+
   }
 
   return result;
-};
+}
 
 console.log(distanceK(
   makeBinaryTree([3,5,1,6,2,0,8,null,null,7,4]),
