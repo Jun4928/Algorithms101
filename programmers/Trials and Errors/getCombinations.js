ac413f0f991eea1918da29abfa92a1d2adbf8005
Jun4928 <jun.choi.4928@gmail.com>
2020-06-16 15:01:51 +0900

    20.06.16 permutaion, combination algorithm

--- programmers/Trials and Errors/getCombinations.js
+++ programmers/Trials and Errors/getCombinations.js
@@ -1,17 +1,17 @@
-const getCombinations = function (arr, n) {
+const getCombinations = function (arr, selectNumber) {
   const results = [];
-  if (n === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+  if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
 
-  arr.forEach((first, index, origin) => {
-    const rest = origin.slice(index + 1); // 해당하는 인덱스를 제외한 나머지 뒤
-    const combinations = getCombinations(rest, n - 1); // 나머지에 대해서 조합을 구한다.
-    const attached = combinations.map((combination) => [first, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
+    const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+    const attached = combinations.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
     results.push(...attached); // 배열 spread syntax 로 모두다 push
   });
 
   return results; // 결과 담긴 results return
 }
 
-const example = [1,2,3,4,5];
-const result = getCombinations(example, 2);
+const example = [1,2,3,4];
+const result = getCombinations(example, 3);
 console.log(result);
--- programmers/Trials and Errors/getPermutations.js
+++ programmers/Trials and Errors/getPermutations.js
@@ -1,17 +1,17 @@
-const getPermutations= function (arr, n) {
+const getPermutations= function (arr, selectNumber) {
   const results = [];
-  if (n === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+  if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
 
-  arr.forEach((first, index, origin) => {
-    const rest = [...origin.slice(0, index), ...origin.slice(index+1)] // 해당하는 인덱스를 제외한 배열 
-    const permutations = getPermutations(rest, n - 1); // 나머지에 대해 순열을 구한다.
-    const attached = permutations.map((permutation) => [first, ...permutation]); // 돌아온 순열에 대해 떼 놓은(fixed) 값 붙이기
+  arr.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index+1)] // 해당하는 fixed를 제외한 나머지 배열 
+    const permutations = getPermutations(rest, selectNumber - 1); // 나머지에 대해 순열을 구한다.
+    const attached = permutations.map((permutation) => [fixed, ...permutation]); // 돌아온 순열에 대해 떼 놓은(fixed) 값 붙이기
     results.push(...attached); // 배열 spread syntax 로 모두다 push
   });
 
   return results; // 결과 담긴 results return
 };
 
-const example = [1,2,3,4,5];
-const result = getPermutations(example, 2);
+const example = [1,2,3,4];
+const result = getPermutations(example, 3);
 console.log(result);
