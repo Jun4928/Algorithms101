5b6ab2c261f0fb0fb486e0029c4424f204cc3e96
Jun4928 <jun.choi.4928@gmail.com>
2020-06-16 15:43:28 +0900

    20.06.16 subset algorithm improved

--- programmers/Trials and Errors/getSubsets.js
+++ programmers/Trials and Errors/getSubsets.js
@@ -4,11 +4,9 @@ const getSubsets = function (arr) {
 
   const subSet = function DFS (depth) { // 부분 집합 구하는 재귀 함수, DFS 알고리즘
     if (depth === arr.length) { // 트리의 끝에 다다른 것 ==> 재귀 종료 조건
-      const set = [];
-      for (let i = 0; i <= arr.length; i++) { // 이 순간의 flag에 따라 부분집합의 값들이 달라진다.
-        if (flag[i] === true) set.push(arr[i]); // flag === true => 집합 만들기 
-      }
-      subSets.push(set); // 부분집합들을 담는 배열에 push
+      const subSet = arr.filter((value, index) => flag[index]); // 해당 flag true => 부분집합 포함
+      subSets.push(subSet); // 부분집합들을 담는 배열에 push
+
       return;
     }
 
@@ -23,6 +21,6 @@ const getSubsets = function (arr) {
   return subSets;
 }
 
-const example = [1,2,3,4,5];
+const example = [1,2,3];
 const result = getSubsets(example);
 console.log(result);
