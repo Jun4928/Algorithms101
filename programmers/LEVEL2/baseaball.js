c848bd627dac7903197ab3f44f94b75dd0425dee
Jun4928 <jun.choi.4928@gmail.com>
2020-06-16 15:02:55 +0900

    20.06.16 permutation algorithm modified

--- programmers/LEVEL2/baseaball.js
+++ programmers/LEVEL2/baseaball.js
@@ -1,11 +1,11 @@
-const getPermutations= function (arr, n) {
+const getPermutations= function (arr, selectNumber) {
   const results = [];
-  if (n === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+  if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
 
-  arr.forEach((first, index, origin) => {
-    const rest = [...origin.slice(0, index), ...origin.slice(index+1)] // 해당하는 인덱스를 제외한 배열 
-    const permutations = getPermutations(rest, n - 1); // 나머지에 대해 순열을 구한다.
-    const attached = permutations.map((permutation) => [first, ...permutation]); // 돌아온 순열에 대해 떼 놓은(fixed) 값 붙이기
+  arr.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index+1)] // 해당하는 fixed를 제외한 나머지 배열 
+    const permutations = getPermutations(rest, selectNumber - 1); // 나머지에 대해 순열을 구한다.
+    const attached = permutations.map((permutation) => [fixed, ...permutation]); // 돌아온 순열에 대해 떼 놓은(fixed) 값 붙이기
     results.push(...attached); // 배열 spread syntax 로 모두다 push
   });
 
